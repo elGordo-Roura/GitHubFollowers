bbac1c58f6dfb67c507435e6011e4e476383e616
Christopher J. Roura <cjroura@gmail.com>
2022-11-16 17:30:33 -0600

    [NET-03] Create network call

--- GitHubFollowers/Screens/FollowerListViewController.swift
+++ GitHubFollowers/Screens/FollowerListViewController.swift
@@ -13,6 +13,16 @@ class FollowerListViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        guard let username = username else { return }
+        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, error in
+            guard let followers = followers else {
+                self.presentCRAlertOnMainThread(title: "Beep boop beep", message: error!, buttonTitle: "Ok")
+                return
+            }
+            
+            print(followers)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
