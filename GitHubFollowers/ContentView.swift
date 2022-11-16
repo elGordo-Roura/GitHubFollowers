9bbd3360e86910758a448ca3d3b90faf0058bd3a
Christopher J. Roura <chris.roura@favordelivery.com>
2022-11-15 18:49:17 -0600

    Initial Commit

---
+++ GitHubFollowers/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHubFollowers
+//
+//  Created by Christopher J. Roura on 11/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubFollowers/GitHubFollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubFollowersApp.swift
+//  GitHubFollowers
+//
+//  Created by Christopher J. Roura on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubFollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
