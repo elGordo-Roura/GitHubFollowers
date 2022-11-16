8b4814991f000cc7ab90ff1cc23b567b133a4285
Christopher J. Roura <cjroura@gmail.com>
2022-11-16 16:58:44 -0600

    [NET-01] Set up network calls

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Christopher J. Roura on 11/16/22.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
